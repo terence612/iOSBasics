012f54f96f7e09ffb8b2a600dadfbbc5828dcae5
Terlun Leung <terence612@gmail.com>
2018-10-31 00:39:22 +0800

    Initial Commit

---
+++ LearningDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningDemo
+//
+//  Created by Terlun Leung on 31/10/2018.
+//  Copyright © 2018 Terlun Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
